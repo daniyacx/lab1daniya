677613c4c1cb6f5e3d4d8a049da1ed875e706ac2
aubak <aubakirovadania@gmail.com>
2023-04-05 02:00:59 +0600

    a program that finds factorial "n" using recursion

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-//a program that finds factorial "n" using recursion
-//this recursive continues until we reach the base case of 0! = 1.
-//or we use the recursive of n! as n * (n-1)!
+/**
+* a program that finds factorial "n" using recursion
+* this recursive continues until we reach the base case of 0! = 1.
+* or we use the recursive of n! as n * (n-1)!
+ */
 class Factorial {
     public static void main(String[] args) {
         int n = 5;
