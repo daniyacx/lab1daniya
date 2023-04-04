4bbe16bdfc3ad15947dd2e0c327852d1fcb4d9c1
aubak <aubakirovadania@gmail.com>
2023-04-05 02:36:13 +0600

    a program that finds n-th elements in Fibonacci sequence  using recursion

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,25 @@
 /**
-* a program that finds factorial "n" using recursion
-* this recursive continues until we reach the base case of 0! = 1.
-* or we use the recursive of n! as n * (n-1)!
+* a program that finds n-th elements in Fibonacci sequence using recursion
+* we have two base cases that terminate the recursion when n is 0 or 1
+* or we use the recursive definition of the Fibonacci sequence as Fn = Fn-1 + Fn-2
+ * we can call it with any non-negative integer n, and it will recursively calculate the n-th element in the Fibonacci sequence.
  */
-class Factorial {
+public class Main {
     public static void main(String[] args) {
-        int n = 5;
-        int result = factorial(n);
-        System.out.println(n + "! = " + result);
+        int n = 17;
+        int result = fibonacci(n);
+        System.out.println("Fibonacci(" + n + ") = " + result);
     }
 
-    public static int factorial(int n) {
-        if (n == 0) {  // base case: 0! = 1
+    public static int fibonacci(int n) {
+        if (n == 0) {  // base case: F0 = 0
+            return 0;
+        } else if (n == 1) {  // base case: F1 = 1
             return 1;
-        } else {  // recursive case: n! = n * (n-1)!
-            return n * factorial(n-1);
+        } else {  // recursive case: Fn = Fn-1 + Fn-2
+            return fibonacci(n-1) + fibonacci(n-2);
         }
     }
 }
+
 
