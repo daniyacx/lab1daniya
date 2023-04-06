f3707dcb3204191bd9c6fb4dd6b6fcb1f440523e
aubak <aubakirovadania@gmail.com>
2023-04-06 09:32:46 +0600

    the function that finds average from arr (CORRECT VER)

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the length of the array: ");
+        int n = input.nextInt();
 
-        Scanner scanner = new Scanner(System.in);
+        // Create an array to store the user's numbers
+        int[] numbers = new int[n];
 
-        // get the size of the array from the user
-        System.out.print("Enter the size of the array: ");
-        int size = scanner.nextInt();
-
-        int[] arr = new int[size];
-
-        // get the elements of the array from the user
-        System.out.println("Enter the elements of the array: ");
-        for (int i = 0; i < size; i++) {
-            arr[i] = scanner.nextInt();
+        // Ask the user to enter n numbers and store them in the array
+        System.out.println("Enter the " + n + " numbers: ");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = input.nextInt();
         }
 
-        // find the minimum value from the array
-        int min = findMin(arr, size);
+        // Calculate the average of the numbers using recursion
+        double average = calculateAverage(numbers, n);
 
-        // display the minimum value
-        System.out.println("Minimum value from the array is: " + min);
+        // Display the average to the user
+        System.out.println("The average is: " + average);
     }
 
     /**
-     * This method recursively finds the minimum value from an array
-     * @param arr the array to search
-     * @param n the number of elements in the array to search
-     * @return the minimum value from the array
+     * This method calculates the average of an array of numbers using recursion
+     * @param numbers The array of numbers
+     * @param n The length of the array
+     * @return The average of the numbers
      */
-    public static int findMin(int[] arr, int n) {
-        if (n == 1)
-            return arr[0];
-        else {
-            int min = findMin(arr, n - 1);
-            return Math.min(min, arr[n - 1]);
+    public static double calculateAverage(int[] numbers, int n) {
+        if (n == 0) { // If the array is empty, return 0.0
+            return 0.0;
+        } else { // Otherwise, recursively call the method and calculate the average
+            double sum = calculateAverage(numbers, n - 1) * (double)(n - 1);
+            sum += (double)numbers[n - 1];
+            return sum / (double)n;
         }
     }
 }
