98382b56d1095fe61b2f7007fa8e90ebb70c1fe5
aubak <aubakirovadania@gmail.com>
2023-04-08 08:50:33 +0600

    the function that finds minimum from arr (CORRECT VER) pt2

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,8 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a non-negative integer: ");
-        int n = scanner.nextInt();
-        int result = fibonacci(n);
-        System.out.println("Fibonacci(" + n + ") = " + result);
-    }
+        Task1.task1();
 
-    /**
-     * find the nth Fibonacci number using recursion
-     * @param n the index of the desired Fibonacci number (0-based)
-     * @return the value of the nth Fibonacci number
-     */
-    public static int fibonacci(int n) {
-        if (n == 0) {  // base case: F0 = 0
-            return 0;
-        } else if (n == 1) {  // base case: F1 = 1
-            return 1;
-        } else {  // recursive case: Fn = Fn-1 + Fn-2
-            return fibonacci(n-1) + fibonacci(n-2);
-        }
     }
 }
---
+++ src/Task1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void task1() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // get the size of the array from the user
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] arr = new int[size];
+
+        // get the elements of the array from the user
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        // find the minimum value from the array
+        int min = findMin(arr, size);
+
+        // display the minimum value
+        System.out.println("Minimum value from the array is: " + min);
+    }
+
+    /**
+     * This method recursively finds the minimum value from an array
+     * @param arr the array to search
+     * @param n the number of elements in the array to search
+     * @return the minimum value from the array
+     */
+    public static int findMin(int[] arr, int n) {
+        if (n == 1)
+            return arr[0];
+        else {
+            int min = findMin(arr, n - 1);
+            return Math.min(min, arr[n - 1]);
+        }
+    }
+}
